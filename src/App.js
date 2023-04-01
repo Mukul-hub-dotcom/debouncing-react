b71cbc54e9697ff6355e8e648477c11f8b14c088
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-04-01 18:48:35 +0530

    debouncing assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_KEY = 'f9f40125';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    const debouncing = setTimeout(() => {
+      if (search) {
+        setLoading(true);
+        try {
+          axios.get(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+            .then(response => {
+              setMovies(response.data.Search);
+              setLoading(false);
+            });
+        } catch (error) {
+          console.log(error);
+          setLoading(false);
+        }
+      } else {
+        setMovies([]);
+      }
+    }, 1000);
+    
+    return () => clearTimeout(debouncing);
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="Search movies..." onChange={(event) => setSearch(event.target.value)} />
+      {loading && <p>Loading...</p>}
+      {!loading && movies.length > 0 &&
+        <ul>
+          {movies.map(movie => <li key={movie.imdbID}>{movie.Title} ({movie.Year})</li>)}
+        </ul>
+      }
+      {!loading && movies.length === 0 && search &&
+        <p>No movies found.</p>
+      }
     </div>
   );
 }
